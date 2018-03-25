0ee3802b8118de20de2c46dd5e57d42c3bc44e6f
MrRobot420 <max.karlchen@web.de>
2018-03-25 03:52:59 +0200

    # Added score # Added moving enemy & main (controllable) # Added 2 Labels

--- Pong Tut/GameScene.swift
+++ Pong Tut/GameScene.swift
@@ -11,14 +11,82 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ball = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var main = SKSpriteNode()
+    
+    var score = [Int]()
+    
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-       
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+        
+        startGame()
+    }
+    
+    func startGame() {
+        score = [0,0]
+        
+        
+    }
+    
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+        } else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -10, dy: -10))
+        }
+        print(score)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.2))
+        
+        if ball.position.y <= main.position.y - 30 {
+            addScore(playerWhoWon: enemy)
+        } else if ball.position.y >= enemy.position.y + 30 {
+            addScore(playerWhoWon: main)
+        }
+        
     }
 }
